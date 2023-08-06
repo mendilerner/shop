64b3094ab8ef4d72e00f9c452a6f1793c4b7e5ae
mendi <mendi3266@gmail.com>
2023-08-06 12:41:07 +0300

    create nav_br for home page in js, the links stil does not working

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
 import data from './data.js'
 import createHeader from './../header/header.js'
+import createNavBer from './nav_br.js'
+let main = document.querySelector('main')
+let navBr = createNavBer()
+main.append(navBr)
---
+++ js/nav_br.js
@@ -0,0 +1,35 @@
+function createNavBer(){
+    let nav = document.createElement('nav')
+    let ul = document.createElement('ul')
+    const listItems = ['All Products', 'Men', 'Women', 'Jewelry','Electronics']
+    for(let item of listItems){
+        let li = document.createElement('li')
+        li.textContent = item
+        ul.append(li)
+    }
+    let searchBox = document.createElement('div') 
+    let form = document.createElement('form')
+    let searchInput = document.createElement('input')
+    let searchBtn = document.createElement('button')
+    let icon = document.createElement('i')
+    searchBtn.appendChild(icon)
+    form.append(searchInput, searchBtn)
+    searchBox.appendChild(form)
+
+    // add class
+    searchBox.classList.add('search-box')
+    form.classList.add('search-bar')
+    icon.classList.add('material-icons')
+
+    // add some attribute
+    searchInput.type = 'text'
+    searchInput.placeholder = 'Search'
+    icon.innerHTML = 'search'
+
+    // add to nav
+    nav.append(ul, searchBox)
+    return nav
+}
+
+export default createNavBer;
+
